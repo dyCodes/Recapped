d9db9bb732ec1d74eb71447a043c145f1463590c
dyCodes <yusufdaudu51@gmail.com>
2024-03-13 02:09:59 +0100

    feat: setup project folder

---
+++ assets/data-demo.js
@@ -0,0 +1,112 @@
+// App demo data
+const marketPlaceData = [
+	{
+		id: 1,
+		name: "Alex Okekan",
+		quantity: "> 50 large bottles",
+		location: "Jakande Market",
+	},
+	{
+		id: 2,
+		name: "Joseph Ike",
+		quantity: "< 100 small bottles",
+		location: "Jakande Market",
+	},
+	{
+		id: 3,
+		name: "Mercy Kate",
+		quantity: "> 45 large bottles",
+		location: "Alaba Market",
+	},
+	{
+		id: 4,
+		name: "Sean Ope",
+		quantity: "> 30 large bottles",
+		location: "Jankara Market",
+	},
+	{
+		id: 5,
+		name: "Rita Ora",
+		quantity: "> 65 small bottles",
+		location: "Balogun Market",
+	},
+	{
+		id: 6,
+		name: "Josh Funke",
+		quantity: "< 200 small bottles",
+		location: "Jakande Market",
+	},
+	{
+		id: 7,
+		name: "Ola Damini",
+		quantity: "< 120 small bottles",
+		location: "Oshodi Market",
+	},
+	{
+		id: 8,
+		name: "Chuka Ope",
+		quantity: "> 150 large bottles",
+		location: "Janaweri Market",
+	},
+];
+
+const HistoryData = [
+	{
+		id: 1,
+		name: "Mercy Kate",
+		status: "Successful",
+		quantity: "120",
+		points: 12,
+		date: "5/27/2023",
+	},
+	{
+		id: 2,
+		name: "Alex Okekan ",
+		status: "Successful",
+		quantity: "65",
+		points: 6.5,
+		date: "5/21/2023",
+	},
+	{
+		id: 3,
+		name: "Rita Ora",
+		status: "Successful",
+		quantity: "150",
+		points: 15,
+		date: "5/18/2023",
+	},
+	{
+		id: 4,
+		name: "Henry Sauss",
+		status: "Successful",
+		quantity: "85",
+		points: 8.5,
+		date: "5/12/2023",
+	},
+	{
+		id: 5,
+		name: "Ade joss",
+		status: "Successful",
+		quantity: "180",
+		points: 18,
+		date: "5/19/2023",
+	},
+	{
+		id: 6,
+		name: "Ngozi Nke",
+		status: "Cancelled",
+		quantity: "200",
+		points: 0,
+		date: "5/08/2023",
+	},
+	{
+		id: 7,
+		name: "Bill Harrison",
+		status: "Cancelled",
+		quantity: "95",
+		points: 0,
+		date: "5/21/2023",
+	},
+];
+
+export { marketPlaceData, HistoryData };
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css';
+import { Poppins } from 'next/font/google';
+import Head from 'next/head';
+
+const font = Poppins({ weight: ['300', '400', '500', '600', '700'], subsets: ['latin'] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>Recapped</title>
+				<meta
+					name='description'
+					content='Recapped - Recycle plastic bottles by donating to market merchants while earning sustainability points'
+				/>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<div className={font.className}>
+				<Component {...pageProps} />
+			</div>
+		</>
+	);
 }
